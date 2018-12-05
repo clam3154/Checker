5d724a52de9bac1f69274e2dced1951b7a557567
BT_1N3_12@bktech.local <BT_1N3_12@bktech.local>
2018-12-05 09:27:17 -0500

    Initial commit

---
+++ src/SubstringChecker.java
@@ -0,0 +1,12 @@
+public class SubstringChecker implements Checker
+{
+    private String goalString;
+    public SubstringChecker(String goal)
+    {
+        goalString = goal;
+    }
+    public boolean accept(String text)
+    {
+        return (text.indexOf(goalString) != -1);
+    }
+}
